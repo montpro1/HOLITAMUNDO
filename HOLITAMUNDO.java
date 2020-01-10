498acce4e174e2f154685d24d5fe0efa33ff50eb
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2020-01-10 14:11:50 +0100

    como estamos

--- HOLITAMUNDO.java
+++ HOLITAMUNDO.java
@@ -6,7 +6,7 @@ public class HOLITAMUNDO {
 
   
     public static void main(String[] args) {
-        //System.out.println("HOLITA MUNDO");
+        //System.out.println("HOLITA MUNDO como estamos");
         
     }
     
